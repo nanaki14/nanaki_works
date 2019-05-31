a13b41d8d61c87b66f1067af58b7e6a20ed1f558
nanaki14 <masatochiba0122@gmail.com>
2019-05-31 12:15:52 +0900

    test: 💍 wip

--- assets/javascript/works.js
+++ assets/javascript/works.js
@@ -1,38 +1,38 @@
 const works = [
   {
-    thumb: '/images/noise_color_thumb.png?190531',
+    thumb: '/images/noise_color_thumb.png',
     link: 'https://vimeo.com/333936138'
   },
   {
-    thumb: '/images/lorem_thumb.png?190531',
+    thumb: '/images/lorem_thumb.png',
     link: 'https://vimeo.com/333936053'
   },
   {
-    thumb: '/images/noise_motion_thumb.png?190531',
+    thumb: '/images/noise_motion_thumb.png',
     link: 'https://vimeo.com/333936199'
   },
   {
-    thumb: '/images/path_glitch_thumb.png?190531',
+    thumb: '/images/path_glitch_thumb.png',
     link: 'https://vimeo.com/333936440'
   },
   {
-    thumb: '/images/path_motion_thumb.png?190531',
+    thumb: '/images/path_motion_thumb.png',
     link: 'https://vimeo.com/333936510'
   },
   {
-    thumb: '/images/triangle_animation_thumb.png?190531',
+    thumb: '/images/triangle_animation_thumb.png',
     link: 'https://vimeo.com/333936656'
   },
   {
-    thumb: '/images/circle_animation_thumb.png?190531',
+    thumb: '/images/circle_animation_thumb.png',
     link: 'https://vimeo.com/333935802'
   },
   {
-    thumb: '/images/shape_anim_thumb.png?190531',
+    thumb: '/images/shape_anim_thumb.png',
     link: 'https://vimeo.com/333936550'
   },
   {
-    thumb: '/images/shape_motion_thumb.png?190531',
+    thumb: '/images/shape_motion_thumb.png',
     link: 'https://vimeo.com/333936637'
   }
 ]
