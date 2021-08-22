15bf447ff53ea810e4638be8ccef419a70226863
nanaki14 <nanaki.works@gmail.com>
2021-08-22 11:03:00 +0900

    init next

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  webpack5: true,
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+       test: /\.(glsl|frag|vert)$/,
+      use: [
+        options.defaultLoaders.babel,
+          { loader: "raw-loader" },
+          { loader: "glslify-loader" },
+      ],
+    })
+
+    return config
+  },
+}
--- plugins/delay.js
+++
@@ -1,4 +0,0 @@
-export default (context, inject) => {
-  const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-  inject('delay', delay)
-}
